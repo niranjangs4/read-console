e0357e25e9bc06f560e931bad4c5c0efbc7a5df7
Niranjan Kumar G S <niranjan4@outlook.in>
2018-05-07 22:42:25 +0530

    Update console.py

--- .gitignore/console.py
+++ .gitignore/console.py
@@ -2,11 +2,11 @@ import io
 import time
 import subprocess
 import sys,os
-os.chdir(r"C:\Program Files\Intel Corporation\Intel(R)SystemCyclingUtility")
+
 
 filename = 'test.log'
 with io.open(filename, 'wb') as writer, io.open(filename, 'rb', 1) as reader:
-    process = subprocess.Popen('"Intel(R)SystemCyclingUtility.exe"', stdout=writer, shell = True)
+    process = subprocess.Popen('"tool"', stdout=writer, shell = True)
     while process.poll() is None:
         sys.stdout.write(reader.read())
         time.sleep(0.5)
